39bc1315402e50a0e462045fd1e5f4fd4cc558b6
KirillLutsenko <mnrovski68@gmail.com>
2021-05-31 22:21:08 +0300

    Split each object with a period into objects with each date, deleted objects in which 0 tons were transported, combined objects in which the ID and date coincide (having flattened tons in them)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,114 @@
+import React from 'react';
 import 'App.scss';
+import { DB } from './api';
+const moment = require('moment');
 
 function App() {
+  let allDaysDB = [];
+  let finalDB = [];
+
+  const formatTheDate = function(date) {
+    return moment()
+    .format(date)
+    .split('/')
+    .reverse()
+    .join('-');
+  }
+
+  const getDates = function(startDate, endDate) {
+    let dateArray = [];
+    let currentDate = moment(startDate);
+    const stopDate = moment(endDate);
+
+    while (currentDate <= stopDate) {
+        dateArray.push( moment(currentDate).format('YYYY-MM-DD dddd') )
+        currentDate = moment(currentDate).add(1, 'days');
+    }
+    return dateArray;
+  }
+
+
+  DB.forEach(object => {
+    const startDate = formatTheDate(object.startDate);
+    const endDate = formatTheDate(object.endDate);
+    const datesArray = getDates(startDate, endDate);
+    let daysCount = 0;
+    const totalTonns = object.tonns;
+    let fullWeekArray = [];
+    let fiveDaysWeekArray = [];
+
+    if(!object.weekend) {
+      const filterDatesArray = datesArray
+        .filter(date => !(date.includes('Sunday') || date.includes('Saturday')));
+      
+      fiveDaysWeekArray = filterDatesArray.map(date => {
+        daysCount = filterDatesArray.length;
+        object.startDate = date;
+        object.endDate = date;
+        object.tonns = Number((totalTonns / daysCount).toFixed(2));
+        
+        return Object.assign({}, object)
+      })
+
+      allDaysDB = [...allDaysDB, ...fiveDaysWeekArray]
+    }
+
+    if(object.weekend) {
+      fullWeekArray = datesArray.map(date => {
+        daysCount = datesArray.length;
+        object.startDate = date;
+        object.endDate = date;
+        object.tonns = Number((totalTonns / daysCount).toFixed(2));
+
+        return Object.assign({}, object)
+      })
+
+      allDaysDB = [...allDaysDB, ...fullWeekArray]
+    }
+  });
+
+  const mapWithoutRecurringDates = new Map();
+
+  allDaysDB.forEach(entry => {
+    const currentObj = {...entry}
+    const id = `${currentObj.startDate} ${currentObj.resourceId}`;
+    
+    if(mapWithoutRecurringDates.has(id)) {
+      currentObj.tonns += mapWithoutRecurringDates.get(id).tonns;
+    } 
+
+    mapWithoutRecurringDates.set(id, currentObj);
+  })
+
+  finalDB = Array.from(mapWithoutRecurringDates.values());
+
+  //---------solution with Object-------------------
+
+  // const objectWithoutRecurringDates = {};
+
+  // allDaysDB.forEach(entry => {
+  //   const currentObj = {...entry}
+  //   const id = `${currentObj.startDate} ${currentObj.resourceId}`;
+
+  //   if(objectWithoutRecurringDates.hasOwnProperty(id)) {
+  //     currentObj.tonns += objectWithoutRecurringDates[id].tonns;
+  //   }
+    
+  //   objectWithoutRecurringDates[id] = currentObj;
+  // })
+
+  // finalDB = Object.values(objectWithoutRecurringDates);
+
+  //---------solution with Object-------------------
+
+  console.log(finalDB);
+
   return (
     <div className="App">
-      <h1>Hello World.</h1>
+      <h1>Objects</h1>
     </div>
   );
 }
 
 export default App;
+
---
+++ src/api.js
@@ -0,0 +1,44 @@
+export const DB = [
+  {
+    resourceId: "DOR",
+    startDate: "01/01/2021",
+    endDate: "10/01/2021",
+    tonns: 110,
+    weekend: false,
+  },
+  {
+    resourceId: "DOR",
+    startDate: "05/01/2021",
+    endDate: "10/01/2021",
+    tonns: 60,
+    weekend: false,
+  },
+  {
+    resourceId: "DOR",
+    startDate: "15/01/2021",
+    endDate: "20/01/2021",
+    tonns: 60,
+    weekend: false,
+  },
+  {
+    resourceId: "VAN",
+    startDate: "01/01/2021",
+    endDate: "10/01/2021",
+    tonns: 110,
+    weekend: true,
+  },
+  {
+    resourceId: "VAN",
+    startDate: "05/01/2021",
+    endDate: "10/01/2021",
+    tonns: 60,
+    weekend: true,
+  },
+  {
+    resourceId: "VAN",
+    startDate: "15/01/2021",
+    endDate: "20/01/2021",
+    tonns: 60,
+    weekend: true,
+  },
+];
